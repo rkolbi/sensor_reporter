5bd78203d19e20db88f1cfb7321f668375c9be3b
RKolbi <deepsiks@gmail.com>
2019-05-04 21:49:25 +0900

    Update graph.php

--- graph.php
+++ graph.php
@@ -1,80 +1,84 @@
 <html>
-  <head>
-    <script type="text/javascript"
-            src="helpers/dygraph.js">
-    </script>
-    <link rel="stylesheet" src="helpers/dygraph.css" />
-  </head>
-  <body>
-    <br>
-    <b>
-      <a href="ark/lf.csv">Sensors 1 though 6
-      </a>
-      <br>
-    </b>
-    <div id="graphdiv2"
-         style="width:1000px; height:300px;">
-    </div>
-    <?PHP
-$file = escapeshellarg("ark/p1f.csv"); // for the security concious (should be everyone!)
+<head>
+<script type="text/javascript"
+  src="helpers/dygraph.js"></script>
+<link rel="stylesheet" src="helpers/dygraph.css" />
+</head>
+<body>
+<big><b><center>SensorReport 1</center></b></big>
+
+
+<br><b><a href="ark/volts.csv">Volts Data</a><br></b>
+<div id="graphdiv2"
+  style="width:1000px; height:150px;"></div>
+  
+<br><b><a href="ark/orientation.csv">Orientation Data</a><br></b>
+<div id="graphdiv4"
+  style="width:1000px; height:150px;"></div>
+  
+      
+<?PHP
+$file = escapeshellarg("ark/p1f.csv");
 $linep1f = `tail -n 1 $file`; ?>
-    <br>
-    <b>
-      <a href="ark/p1f.csv">Power (V)
-      </a>
-      <br>
-    </b>
-    <div id="graphdiv3"
-         style="width:1000px; height:100px;">
-    </div>
-    <?PHP
-$file = escapeshellarg("ark/p2f.csv"); // for the security concious (should be everyone!)
+
+<br><b><a href="ark/temperature.csv">Temperature Data</a><br></b>
+<div id="graphdiv3"
+  style="width:1000px; height:50px;"></div>
+
+<br><b><a href="ark/p1f.csv">Power (V)</a><br></b>
+<div id="graphdiv5"
+  style="width:1000px; height:50px;"></div>
+
+<?PHP
+$file = escapeshellarg("ark/p2f.csv");
 $linep2f = `tail -n 1 $file`; ?>
-    <br>
-    <b>
-      <a href="ark/p2f.csv">Power (%)
-      </a>
-      <br>
-    </b>
-    <div id="graphdiv4"
-         style="width:1000px; height:100px;">
-    </div>
-    <script type="text/javascript">
-      g2 = new Dygraph(
-        document.getElementById("graphdiv2"),
-        "ark/lf.csv", // path to CSV file
-        {
-        }
-        // options
-      );
-    </script>
-    <script type="text/javascript">
-      g2 = new Dygraph(
-        document.getElementById("graphdiv3"),
-        "ark/p1f.csv", // path to CSV file
-        {
-        }
-        // options
-      );
-    </script>
-    <script type="text/javascript">
-      g2 = new Dygraph(
-        document.getElementById("graphdiv4"),
-        "ark/p2f.csv", // path to CSV file
-        {
-        }
-        // options
-      );
-    </script>
-    <?PHP
-$file = escapeshellarg("ark/system.csv"); // for the security concious (should be everyone!)
+<br><b><a href="ark/p2f.csv">Power (%)</a><br></b>
+
+    <div id="graphdiv6"
+  style="width:1000px; height:50px;"></div>
+<script type="text/javascript">
+  g2 = new Dygraph(
+    document.getElementById("graphdiv2"),
+    "ark/volts.csv", // path to CSV file
+    {}          // options
+  );
+</script>
+<script type="text/javascript">
+  g2 = new Dygraph(
+    document.getElementById("graphdiv3"),
+    "ark/temperature.csv", // path to CSV file
+    {}          // options
+  );
+</script>
+<script type="text/javascript">
+  g2 = new Dygraph(
+    document.getElementById("graphdiv4"),
+    "ark/orientation.csv", // path to CSV file
+    {}          // options
+  );
+</script>
+<script type="text/javascript">
+  g2 = new Dygraph(
+    document.getElementById("graphdiv5"),
+    "ark/p1f.csv", // path to CSV file
+    {}          // options
+  );
+</script>
+<script type="text/javascript">
+  g2 = new Dygraph(
+    document.getElementById("graphdiv6"),
+    "ark/p2f.csv", // path to CSV file
+    {}          // options
+  );
+</script>
+<?PHP
+$file = escapeshellarg("ark/system.csv");
 $line = `tail -n 1 $file`;
 $lines = (explode(",",$line));
 echo "\n<br>Last system message received at " . $lines[0] . ", " . $lines[1];
 $linesp1f = (explode(",",$linep1f));
 $linesp2f = (explode(",",$linep2f));
-echo "\n<br>Last publish received at " . $linesp1f[0] . ".\n<br>Volts: " . $linesp1f[1] . "- SoC: " . $linesp2f[1];
+echo "\n<br>Last publish received at " . $linesp1f[0] . ".  Volts: " . $linesp1f[1] . "- SoC: " . $linesp2f[1];
 ?>
-  </body>
+</body>
 </html>
-
